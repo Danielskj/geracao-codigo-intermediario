b454d07efd7fcd9d82972af74b1ce4987925698b
Danielskj <carlosdiel16@gmail.com>
2024-02-20 14:47:43 -0300

    att Parser

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -13,6 +13,7 @@ public class Parser {
     private Token currentToken;
     private Token peekToken;
     private StringBuilder xmlOutput = new StringBuilder();
+    private VMWriter vmWriter = new VMWriter();
 
     public Parser(byte[] input) {
         scan = new Scanner(input);
@@ -376,5 +377,9 @@ public class Parser {
             expectPeek(SEMICOLON);
             printNonTerminal("/varDec");
         }
+        
+        public String VMOutput() {
+            return vmWriter.vmOutput();
+    }
 
 }
